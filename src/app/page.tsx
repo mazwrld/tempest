4afdb517271c13e624bd3b386e037d8fd5682e49
Maz <mazspm@icloud.com>
2024-04-07 18:41:34 -0500

    removed unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { CommandInput } from "cmdk";
 import { useEffect, useState } from "react";
+import { Command } from "~/components/ui/command";
 
 export default function HomePage() {
   const [input, setInput] = useState<string>("");
@@ -21,12 +23,32 @@ export default function HomePage() {
   }, [input]);
   return (
     <main className="grainy h-screen w-screen">
-      <input
-        className="text-zinc-900"
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
+      <div className="animate-in animate fade-in-5 slide-in-from-bottom-2.5 flex flex-col items-center gap-6 pt-32 duration-500">
+        <h1 className="text-5xl font-bold tracking-tight">Tempest Search</h1>
+        <p className="max-w-prose text-center text-lg text-zinc-600">
+          A blazing fast search engine for countries around the world.
+          <br /> Start typing a country name to and get a result in
+          milliseconds.
+        </p>
+
+        <div className="w-full max-w-md">
+          <Command>
+            <CommandInput
+              value={input}
+              onValueChange={setInput}
+              placeholder="Search countries..."
+              className="placeholder:text-zinc-500"
+            />
+          </Command>
+        </div>
+
+        <input
+          className="text-zinc-900"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
     </main>
   );
 }
